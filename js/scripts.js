1a4dc2a16b82c477bb60b0c241643976c2f964c7
Bartłomiej Świątek <emini1@swiatek.biz>
2015-10-05 23:14:08 +0200

    podpisy pod zdjęciami na SG

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,12 @@
             $(".gallery-frame li[style='display: block;'] img").click();
         },7000);
 
+
+
+        $('.galleryformatter-greenarrows .overlay-inner h4').append('<a href="#" class="viewfull"></a>');
+        $('.galleryformatter-greenarrows .viewfull').on( "click", function(){
+           $(this).parents("li.gallery-slide").find("span.view-full").click();
+        });
     });
 
 })(jQuery, Drupal);
